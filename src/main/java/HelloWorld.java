bdfa8d58e4182be69306cef67e3c1cf6268e1cfb
NickYarmoshyk <acket211@gmail.com>
2021-09-04 23:54:00 +0300

    changed HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,7 @@ package main.java;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GOIT World");
+        System.out.println("Our second commit");
+        System.out.println("Our second commit");
     }
 }
